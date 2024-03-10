ba8f178dd9ffb0bd2d1db26478f1d85fb89ee6ed
Marcinsirant <40653886+Marcinsirant@users.noreply.github.com>
2024-03-10 12:29:32 +0100

    Delete main/java/pl/sirant directory

--- main/java/pl/sirant/price/exception/OldPriceException.java
+++
@@ -1,8 +0,0 @@
-package pl.sirant.price.exception;
-
-public class OldPriceException extends RuntimeException {
-
-  public OldPriceException() {
-    super("Price is older than actual");
-  }
-}
--- main/java/pl/sirant/price/exception/PriceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package pl.sirant.price.exception;
-
-public class PriceNotFoundException extends RuntimeException {
-  public PriceNotFoundException(String instrumentName) {
-    super("Price for instrument " + instrumentName + " not found");
-  }
-}
